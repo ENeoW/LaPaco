114bff5aadd3f3d59f03f92083fd325733844558
PRICETRAVEL\eloy.pedroso <eloy.pedroso@pricetravel.com>
2018-01-23 11:55:36 -0500

    Initial commit

---
+++ LaPaco.DTO/Models/Package.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LaPaco.DTO.Models
+{
+    class Package
+    {
+      public int PackageId { get; set; }
+
+      public string Name { get; set; }
+
+      public DateTime DateOfTrip { get; set; }
+
+      public string CountryFrom { get; set; }
+
+      public string Description { get; set; }
+   }
+}
